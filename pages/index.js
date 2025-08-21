7634e1365c6a60b5de0372f7ce9a97bc19c21299
Sage August Brandtzæg <sagbransgmail.com>
2025-08-21 12:54:29 +0200

    Sage er meg

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Hei, jeg er ge Sage Brandtzæg!</h1>
+      <h1 className="text-4xl font-bold">Hei, jeg er Sage Brandtzæg!</h1>
     </main>
   )
 }
